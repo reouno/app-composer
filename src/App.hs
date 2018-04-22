efee98a603eb57c8b372d9ad48ee3b281822f0bc
Leo Uno <leouno12@gmail.com>
2018-04-22 16:04:03 +0900

    リファクタリング

--- src/App.hs
+++ src/App.hs
@@ -1,34 +1,41 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module App where
 
-import Adapter (adapter)
 import BindType (Bind)
 import Formatter (Formatter)
-import Node (Module(..))
+import Node (Module(..), End(..), Node)
 import TmpIoJson
 import qualified Node as N
 
-type App = [Module]
+data Node' = forall a. (Node a, Show a) => Node' a
+
+instance Show Node' where
+    show (Node' n) = show n
+
+type App = [Node']
+
+runNode :: Node' -> String -> IO String
+runNode (Node' n) = N.run n
 
 run :: App -> String -> IO String
 run [] _ = return ""
-run [m] input = do
-    putStrLn ("Run " ++ show (N.modPath m))
-    --putStrLn ("input:  " ++ show input ++ "\n")
-    N.run m (adapter (N.formatter m) input)
-run (m:ms) input = do
-    putStrLn $ "Run " ++ show (N.modPath m)
-    --putStrLn $ "input:  " ++ show input ++ "\n"
-    input' <- N.run m $ adapter (N.formatter m) input
-    run ms input'
+run [m] input = runNode m input
+run (m:ms) input = run ms =<< runNode m input
 
 mkApp :: [(Bind, String)] -> [TmpIoJson] -> [Formatter] -> App
 mkApp [] [] [] = []
-mkApp graphs ioJsons formatters =
-    map (uncurry3 mkModule) $ zip3 graphs ioJsons formatters
+mkApp graphs ioJsons formatters = modules ++ [endNode]
+    where
+        --modules = map (Node' . uncurry3 mkModule) $ zip3 graphs ioJsons formatters
+        modules =
+            map (Node' . uncurry3 mkModule) $ zip3 (init graphs) (init ioJsons) (init formatters)
+        endNode =
+            Node' $ End (input (last ioJsons)) (output (last ioJsons)) (last formatters)
 
 mkModule :: (Bind, String) -> TmpIoJson -> Formatter -> Module
-mkModule (bind, modPath) ioJson formatter =
-    Module bind modPath (input ioJson) (output ioJson) formatter
+mkModule (bind, modPath) ioJson =
+    Module bind modPath (input ioJson) (output ioJson)
 
 uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
 uncurry3 f (a,b,c) = f a b c
--- src/Node.hs
+++ src/Node.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedLabels #-}
 
 module Node where
 
+import Adapter (adapter, decodeJStr2Str)
 import BindType (Bind)
 import Formatter (Formatter, InputFormat, OutputFormat)
 import System.Exit (ExitCode(..))
 import System.Process (readProcessWithExitCode)
+
+-- appのノード
+class Node a where
+    run :: a -> String -> IO String
 
 data Module = Module
     { bind :: Bind
@@ -15,20 +22,36 @@ data Module = Module
     , formatter :: Formatter
     } deriving (Show, Eq)
 
-class Node a where
-    run :: a -> String -> IO String
-
 instance Node Module where
     run module_ input = runModule (modPath module_) input'
         where
-            input' = format (formatter module_) input
+            input' = adapter (formatter (module_ :: Module)) input
+
+-- 今の所は、最後のモジュールの出力をそのままappの出力とするが、
+-- 将来的に出力形式も指定できるようにするため、フィールドを作っておく
+data End = End
+    { inType :: InputFormat
+    , outType :: OutputFormat
+    , formatter :: Formatter
+    } deriving (Show, Eq)
+
+instance Node End where
+    run endNode input = runEnd $ adapter (formatter (endNode :: End)) input
 
 runModule :: String -> String -> IO String
 runModule modPath input = do
+    putStrLn $ "Run " ++ show modPath
+    --putStrLn $ "input:  " ++ show input
     (status, stdout, stderr) <- readProcessWithExitCode modPath [input] ""
+    --putStrLn $ "output:  " ++ show stdout ++ "\n"
     case status of
         ExitSuccess -> return stdout
         ExitFailure n -> error ("process exit with status code " ++ show n ++ "\n" ++ "error message:\n" ++ stderr)
 
-format :: Formatter -> String -> String
-format rule input = input
+runEnd :: String -> IO String
+runEnd input = do
+    putStrLn "End"
+    -- decodeJStr2Strは可視性のために使っているだけで、必須ではない
+    let output = decodeJStr2Str input
+    putStrLn output
+    return output
